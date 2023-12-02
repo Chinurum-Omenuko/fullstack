46add9f70cdafdf903cbc4d6a749d8a061eda198
chinurum-omenuko <omen0003@algonquinlive.com>
2023-12-02 03:39:15 -0500

    gets and filters fire location and coordinates

--- djangobe/map/views.py
+++ djangobe/map/views.py
@@ -6,9 +6,16 @@ from json.decoder import JSONDecodeError
 async def get_event(request):
    url = "https://eonet.gsfc.nasa.gov/api/v2.1/events"
    async with httpx.AsyncClient() as client:
-       response = await client.get(url)
+       data = await client.get(url)
        try:
-           events = response.json()
+        url = "https://eonet.gsfc.nasa.gov/api/v2.1/events"
+        data = data.json()
+        filtered_data = [
+            {'location': event['title'],
+             'coordinates': event['geometries'][0]['coordinates']
+            }
+            for event in data['events'] for category in event['categories'] if category['title'] == 'Wildfires'
+        ]
        except JSONDecodeError:
            return JsonResponse({"error": "Invalid JSON response from server"}, status=400)
-       return JsonResponse(events)
+       return JsonResponse(filtered_data)
